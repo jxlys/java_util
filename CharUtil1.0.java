3c2ec56c283e22353a3b6e5e91caf5127750c5df
jxlys <34503580+jxlys@users.noreply.github.com>
2017-12-13 17:17:42 +0800

    Create CharUtil1.0.java

---
+++ CharUtil1.0.java
@@ -0,0 +1,59 @@
+package cn.util;
+
+/**
+ * 字符工具类: 一些简单的操作方法。
+ * 
+ * @author jxlys @version1.0
+ */
+public class CharUtil {
+
+	/**
+	 * 返回字符对应的int值.
+	 * 
+	 * @param c
+	 */
+	public static int getInt(char c) {
+		return (int) c;
+	}
+
+	/**
+	 * 检测是否字母
+	 * 
+	 * @param c
+	 * @return
+	 */
+	public static boolean isLetterCharacter(char c) {
+		return isUpperCharacter(c) || isLowerCharacter(c) ? true : false;
+	}
+
+	/**
+	 * 检测大写
+	 * 
+	 * @param c
+	 */
+	public static boolean isLowerCharacter(char c) {
+		return (c >= 97 && c < 123) ? true : false;
+	}
+
+	/**
+	 * 检测是否数字.
+	 * 
+	 * @param c
+	 */
+	public static boolean isNumber(char c) {
+		return c >= 48 && c < 58 ? true : false;
+	}
+
+	/**
+	 * 检测小写.
+	 * 
+	 * @param c
+	 */
+	public static boolean isUpperCharacter(char c) {
+		return (c >= 65 && c < 91) ? true : false;
+	}
+
+	private CharUtil() {// 不允许使用构造方法
+		throw new RuntimeException();
+	}
+}
